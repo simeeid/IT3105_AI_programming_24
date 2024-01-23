3227cf1efd246671a70fec90c2205b854bc303c4
Simen <simeeid@stud.ntnu.no>
2024-01-23 15:00:21 +0100

    Added room temperature plant heh

---
+++ project_code/room_temperature_plant.py
@@ -0,0 +1,17 @@
+#from abstract_plant import AbstractPlant
+
+import numpy as np
+import jax
+import jax.numpy as jnp
+
+class RoomTemperaturePlant():#AbstractPlant):
+    def __init__(self, temperature_outside, heat_transfer_coefficient, volume):
+        self.temperature_outside = temperature_outside + 273.15 # K
+        self.heat_transfer_coefficient = heat_transfer_coefficient
+        self.volume = volume
+        self.density = 1.2 # kg/m^3
+        self.specific_heat_capacity = 1005 # J/(kg*K)
+
+    def update_plant(self, control_signal, external_disturbance, temperature_inside):
+        temperature_inside = temperature_inside + (control_signal / (self.density * self.specific_heat_capacity * self.volume)) * (self.temperature_outside - temperature_inside) + control_signal / (self.density * self.specific_heat_capacity * self.volume) + external_disturbance
+        return temperature_inside
